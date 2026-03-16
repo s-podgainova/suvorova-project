8a40b608cff3d36c9b2bbf31981bc857729cba73
SPodgainova <spodgainova@gmail.com>
2026-03-16 14:22:06 +0300

    fix: burger

--- components/BurgerButton/BurgerButton.tsx
+++ components/BurgerButton/BurgerButton.tsx
@@ -10,13 +10,29 @@ export const BurgerButton = ({ isOpen, onClick }: BurgerButtonProps) => {
     <button
       className={`${styles.burger} ${isOpen ? styles.open : ""}`}
       onClick={onClick}
-      aria-label="Открыть меню"
+      aria-label={isOpen ? "Закрыть меню" : "Открыть меню"}
       aria-expanded={isOpen}
       aria-controls="site-menu"
     >
-      <span></span>
-      <span></span>
-      <span></span>
+      <svg
+        className={styles.icon}
+        width="30"
+        height="17"
+        viewBox="0 0 30 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
+        <line className={styles.lineTop} x1="0" y1="0.5" x2="30" y2="0.5" />
+        <line className={styles.lineMiddle} x1="0" y1="7.5" x2="30" y2="7.5" />
+        <line
+          className={styles.lineBottom}
+          x1="0"
+          y1="14.5"
+          x2="30"
+          y2="14.5"
+        />
+      </svg>
     </button>
   );
 };
