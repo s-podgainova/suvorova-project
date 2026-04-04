2585e796184d31254b6ba18e9654fadb14690bbb
SPodgainova <spodgainova@gmail.com>
2026-04-04 18:12:52 +0300

    fix:icons

--- shared/icons/TG.tsx
+++ shared/icons/TG.tsx
@@ -10,8 +10,8 @@ export const TG = () => {
       <path
         d="M11.7143 1.14307L0.285706 5.2247L3.55101 6.85735L8.44897 3.59205L5.18366 7.67368L10.0816 10.939L11.7143 1.14307Z"
         stroke="#1B1B1B"
-        stroke-width="0.5"
-        stroke-linejoin="round"
+        strokeWidth="0.5"
+        strokeLinejoin="round"
       />
     </svg>
   );
